7352f3a627928e4c03adc8e56ae068209299b792
alvaroormeno <alvaro@ormeno.org>
2022-08-28 19:10:49 -0400

    indez.js - imported Provider and wrapped App.js inside it

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 
 // Import Redux Store
-import { store } from './app/store.js'
+import { store } from './app/store.js';
+// Import Provider
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
+    // Wrap the App inside the Provider and now our store / Global State will be available to the app.
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
