52ece7cde4984ac66f4f3d31115d39f7db07e097
alvaroormeno <alvaro@ormeno.org>
2022-08-28 20:42:46 -0400

    completed tutorial, all working

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState} from "react";
 // Import Redux Hooks
 import { useSelector, useDispatch } from "react-redux";
 // Import our actions we created
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
 
 const Counter = () => {
-	// Add our state tot he component
+	// Add our state to the component
 	const count = useSelector((state) => state.counter.count);
 	// Define dispatch
 	const dispatch = useDispatch();
+
+
+    // State for incrementAmount
+    const [incrementAmount, setIncrementAmount] = useState(0);
+
+    // Verify we get a number value
+    const addValue = Number(incrementAmount) || 0;
+
+    const resetAll = () => {
+        setIncrementAmount(0);
+        dispatch(reset());
+    }
 
 	return (
 		<section>
@@ -17,6 +29,17 @@ const Counter = () => {
                 {/* Inside of On click, we have an anonimous function. Inside this function we can call dispatch and inside we pass any of the actions we created. */}
                 <button onClick={() => dispatch(increment())}>+</button>
                 <button onClick={() => dispatch(decrement())}>-</button>
+            </div>
+
+            <input 
+                type="text"
+                value={incrementAmount}
+                onChange={(e) => setIncrementAmount(e.target.value) }
+            />
+
+            <div>
+                <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+                <button onClick={resetAll}>Reset</button>
             </div>
 		</section>
 	);
