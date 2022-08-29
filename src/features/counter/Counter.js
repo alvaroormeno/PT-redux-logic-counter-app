9cea86fd8c0433713f0412d30b24032813efd45e
alvaroormeno <alvaro@ormeno.org>
2022-08-28 20:18:29 -0400

    Counter.js - completed component and imported to app.js to child it

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from "react";
 // Import Redux Hooks
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 // Import our actions we created
-import { increment, decrement } from './counterSlice'
+import { increment, decrement } from "./counterSlice";
 
 const Counter = () => {
+	// Add our state tot he component
+	const count = useSelector((state) => state.counter.count);
+	// Define dispatch
+	const dispatch = useDispatch();
 
-  // Add our state tot he component
-  const count = useSelector((state) => state.counter.count);
-  // Define dispatch
-  const dispatch = useDispatch();
+	return (
+		<section>
+			<p>{count}</p>
+            <div>
+                // Inside of On click, we have an anonimous function. Inside this function we can call dispatch and inside we pass any of the actions we created.
+                <button onClick={() => dispatch(incremenet())}>+</button>
+                <button onClick={() => dispatch(decrement())}>-</button>
+            </div>
+		</section>
+	);
+};
 
-  return (
-    <section>
-
-    </section>
-  )
-}
-
-export default Counter
+export default Counter;
